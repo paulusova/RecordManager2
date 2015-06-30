0e3c08a6a723e1c6fe289507771f0d15b00f2990
Milan Janicek <mjtecka@gmail.com>
2015-06-30 23:24:13 +0200

    DublinCoreRecord refactored & methods for all 15 DC elements added

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/dc/DublinCoreRecordImpl.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/dc/DublinCoreRecordImpl.java
@@ -24,18 +24,242 @@ public class DublinCoreRecordImpl implements DublinCoreRecord {
 	private List<String> type;
 
 	public DublinCoreRecordImpl() {
-		creator = new ArrayList();
-		date = new ArrayList();
-		format = new ArrayList();
-		identifier = new ArrayList();
-		title = new ArrayList();
-		type = new ArrayList();
+		/*
+		creator = new ArrayList<String>();
+		date = new ArrayList<String>();
+		format = new ArrayList<String>();
+		identifier = new ArrayList<String>();
+		title = new ArrayList<String>();
+		type = new ArrayList<String>();
+*/
+	}
 
+	
+	/* GET LIST */
+
+	@Override
+	public List<String> getCoverages() {
+		if (this.coverage != null) {
+			return this.coverage;
+		}
+		return new ArrayList<String>();
+	}
+
+	
+	@Override
+	public List<String> getContributors() {
+		if (this.contributor != null) {
+			return this.contributor;
+		}
+		return new ArrayList<String>();
+	}
+	
+	@Override
+	public List<String> getCreators() {
+		if (this.creator != null) {
+			return this.creator;
+		}
+		return new ArrayList<String>();
+	}
+
+	
+	@Override
+	public List<String> getDates() {
+		if (this.date != null) {
+			return this.date;
+		}
+		return new ArrayList<String>();
 	}
 
 	@Override
+	public List<String> getDescriptions() {
+		if (this.description != null) {
+			return this.description;
+		}
+		return new ArrayList<String>();
+	}
+	
+	@Override
+	public List<String> getFormats() {
+		if (this.format != null) {
+			return this.format;
+		}
+		return new ArrayList<String>();
+	}
+	
+	@Override
+	public List<String> getIdentifiers() {
+		if (this.identifier != null) {
+			return this.identifier;
+		}
+		return new ArrayList<String>();
+	}
+
+	@Override
+	public List<String> getLanguages() {
+		if (this.language != null) {
+			return this.language;
+		}
+		return new ArrayList<String>();
+	}
+
+	
+	@Override
+	public List<String> getPublishers() {
+		if (this.publisher != null) {
+			return this.publisher;
+		}
+		return new ArrayList<String>();
+	}
+	
+	@Override
+	public List<String> getRelations() {
+		if (this.relation != null) {
+			return this.relation;
+		}
+		return new ArrayList<String>();
+	}
+	
+	
+	@Override
+	public List<String> getRights() {
+		if (this.rights != null) {
+			return this.rights;
+		}
+		return new ArrayList<String>();
+	}
+	
+	@Override
+	public List<String> getSources() {
+		if (this.source != null) {
+			return this.source;
+		}
+		return new ArrayList<String>();
+	}
+	
+	@Override
+	public List<String> getSubjects() {
+		if (this.subject != null) {
+			return this.subject;
+		}
+		return new ArrayList<String>();
+	}
+	
+	@Override
+	public List<String> getTitles() {
+		if (this.title != null) {
+			return this.title;
+		}
+		return new ArrayList<String>();
+	}
+
+	@Override
+	public List<String> getTypes() {
+		if (this.type != null) {
+			return this.type;
+		}
+		return new ArrayList<String>();
+	}
+
+	
+    /* ADD */
+
+	@Override
+	public void addContributor(String s) {
+		this.contributor = getContributors();
+		contributor.add(s);
+	}
+	
+	@Override
+	public void addCoverage(String s) {
+		this.coverage = getCoverages();
+		coverage.add(s);
+	}
+	
+	@Override
+	public void addCreator(String c) {
+		this.creator = getTitles();
+		creator.add(c);
+	}
+
+	@Override
+	public void addDate(String s) {
+		this.date = getDates();
+		date.add(s);
+	}
+
+	@Override
+	public void addDescription(String s) {
+		this.description = getDescriptions();
+		description.add(s);
+	}
+	
+	@Override
+	public void addFormat(String s) {
+		this.format = getFormats();
+		format.add(s);
+	}
+
+	@Override
+	public void addLanguage(String s) {
+		this.language = getLanguages();
+		language.add(s);
+	}
+	
+	@Override
+	public void addIdentifier(String id) {
+		this.identifier = getIdentifiers();
+		identifier.add(id);
+	}
+
+	@Override
+	public void addPublisher(String s) {
+		this.publisher = getPublishers();
+		publisher.add(s);
+	}
+	
+	@Override
+	public void addRelation(String s) {
+		this.relation = getRelations();
+		relation.add(s);
+		
+	}
+	
+	@Override
+	public void addRights(String s) {
+		this.rights = getRights();
+		rights.add(s);
+	}
+	
+	@Override
+	public void addSources(String s) {
+		this.source = getSources();
+		source.add(s);
+	}
+	
+	@Override
+	public void addSubjects(String s) {
+		this.subject = getSubjects();
+		subject.add(s);
+	}
+	
+	@Override
+	public void addTitle(String t) {
+		this.title = getTitles();
+		title.add(t);
+	}
+
+	@Override
+	public void addType(String s) {
+		this.type = getTypes();
+		type.add(s);
+	}
+
+	/* GET FIRST */
+	
+	@Override
 	public String getFirstCreator() {
-		if (creator.isEmpty()) {
+		if (!getCreators().isEmpty()) {
 			return this.creator.get(0);
 		}
 		return null;
@@ -43,7 +267,7 @@ public class DublinCoreRecordImpl implements DublinCoreRecord {
 
 	@Override
 	public String getFirstDate() {
-		if (!date.isEmpty()) {
+		if (!getDates().isEmpty()) {
 			return this.date.get(0);
 		}
 		return null;
@@ -51,7 +275,7 @@ public class DublinCoreRecordImpl implements DublinCoreRecord {
 
 	@Override
 	public String getFirstFormat() {
-		if (!format.isEmpty()) {
+		if (!getFormats().isEmpty()) {
 			return this.format.get(0);
 		}
 		return null;
@@ -59,7 +283,7 @@ public class DublinCoreRecordImpl implements DublinCoreRecord {
 
 	@Override
 	public String getFirstIdentifier() {
-		if (!identifier.isEmpty()) {
+		if (!getIdentifiers().isEmpty()) {
 			return this.identifier.get(0);
 		}
 		return null;
@@ -68,7 +292,7 @@ public class DublinCoreRecordImpl implements DublinCoreRecord {
 	@Override
 	public String getFirstTitle() {
 
-		if (!title.isEmpty()) {
+		if (!getTitles().isEmpty()) {
 			return this.title.get(0);
 		}
 		return null;
@@ -76,75 +300,12 @@ public class DublinCoreRecordImpl implements DublinCoreRecord {
 
 	@Override
 	public String getFirstType() {
-		if (!type.isEmpty()) {
+		if (!getTypes().isEmpty()) {
 			return this.type.get(0);
 		}
 		return null;
 	}
 
-	@Override
-	public void addCreator(String c) {
-		this.creator.add(c);
-	}
-
-	@Override
-	public void addDate(String s) {
-		this.date.add(s);
-
-	}
-
-	@Override
-	public void addFormat(String s) {
-		this.format.add(s);
-	}
-
-	@Override
-	public void addIdentifier(String id) {
-		this.identifier.add(id);
-
-	}
-
-	@Override
-	public void addTitle(String t) {
-		this.title.add(t);
-	}
-
-	@Override
-	public void addType(String s) {
-		this.type.add(s);
-
-	}
-
-	@Override
-	public List<String> getDates() {
-		if (this.date != null) {
-			return this.date;
-		}
-		return Collections.emptyList();
-	}
-
-	@Override
-	public List<String> getIdentifiers() {
-		if (this.identifier != null) {
-			return this.identifier;
-		}
-		return Collections.emptyList();
-	}
-
-	@Override
-	public List<String> getTitles() {
-		if (this.title != null) {
-			return this.title;
-		}
-		return Collections.emptyList();
-	}
-
-	@Override
-	public List<String> getTypes() {
-		if (this.type != null) {
-			return this.type;
-		}
-		return Collections.emptyList();
-	}
-
+	
+	
 }
